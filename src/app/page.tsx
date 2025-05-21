77b20ecfa37b641efe98b7e01caddb9dda9c6ee4
Aida Kokko <aida.kokko@tuta.io>
2025-05-21 17:56:29 +0300

    Update profile photo to new Aida.jpg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           >
             <div className="relative w-56 h-56 mb-8 rounded-full overflow-hidden ring-4 ring-emerald-500 shadow-xl">
               <Image
-                src="/images/AidaKokko.jpg"
+                src="/images/Aida.jpg"
                 alt="Aida Kokko"
                 width={224}
                 height={224}
